8ae1357037e9e6e56830b15fdbe7f48f8f1cc78d
mbgshk <mbgshk@gmail.com>
2022-07-23 10:14:51 +0700

    Submit tugas

--- headers.cy.js
+++ headers.cy.js
@@ -1,9 +1,20 @@
 /// <reference types="cypress" />
 
 describe('Validate Header', () =>{
-    it.only('Successfully validate content-type', () => {
-        cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
-        cy.get('@pokemon').its('headers').its('content-type')
-            .should('include', 'application/json; charset=utf-8')
-    })
+    // it.only('Successfully validate content-type', () => {
+    //     cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
+    //     cy.get('@pokemon').its('headers').its('content-type')
+    //         .should('include', 'application/json; charset=utf-8')
+    // })
+
+    // Tugas
+    it('Successfully validate body', () => {
+        var ditto = {
+            "name": "imposter"
+        }
+        cy.request('GET', 'https://pokeapi.co/api/v2/pokemon/ditto', ditto).then((response) => {
+            expect(response.status).equal(200)
+            expect(response.body.abilities[1].ability.name).to.eq(ditto.name)
+        })
+    });
 })
