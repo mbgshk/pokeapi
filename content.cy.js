afcfd70a67ea9294c22eb34c86c0195cd49d11ff
mbgshk <mbgshk@gmail.com>
2022-07-25 13:20:30 +0700

    Add Another Code

--- content.cy.js
+++ content.cy.js
@@ -5,4 +5,16 @@ describe('Validate Content', () => {
         cy.request('https://pokeapi.co/api/v2/pokemon/ditto').as('pokemon')
         cy.get('@pokemon').its('body').should('include', {name: "ditto"})
     });
+
+    // Another Code
+    it('Update User', () => {
+        var newUser = {
+            "name": 'mbgshk',
+            "job": 'Test Engineer'
+        }
+        cy.request('PUT', 'https:reqres.in/api/users/2', newUser).then((response) => {
+            expect(response.status).equal(200)
+            expect(response.body.name).to.eq(newUser.name)
+        })
+    });
 })
