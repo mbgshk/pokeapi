828b7e2794ae1234da8e42df59b22d30031a52b8
mbgshk <mbgshk@gmail.com>
2022-07-25 13:36:46 +0700

    Submit tugas

--- content.cy.js
+++ content.cy.js
@@ -17,4 +17,16 @@ describe('Validate Content', () => {
             expect(response.body.name).to.eq(newUser.name)
         })
     });
+
+    // Tugas
+    it('Sucessfully validate ability', () => {
+        var ditto = {
+            "name": "limber",
+            "url": "https://pokeapi.co/api/v2/ability/7/"
+        }
+        cy.request('GET', 'https://pokeapi.co/api/v2/pokemon/ditto', ditto).then((response) => {
+            expect(response.body.abilities[0].ability.name).to.eq(ditto.name)
+            expect(response.body.abilities[0].ability.url).to.eq(ditto.url)
+        })
+    });
 })
